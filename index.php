4c2de1f7a645838415381f217fa3afa891a14d8e
Anca Comanescu <comanesa@mskcc.org>
2021-09-16 11:37:41 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+use Coveo\Search\SDK\SDKPushPHP\Constants;
+use Coveo\Search\SDK\SDKPushPHP\Push;
+
+
+print Push::class . PHP_EOL;
+
+print Constants::class . PHP_EOL;
+
+print Constants::DATE_FORMAT_STRING . PHP_EOL;
