7d936d0ea6c48257e4a39ebfc9ed1ba815b5dc57
Anca Comanescu <comanesa@mskcc.org>
2021-09-17 12:54:08 -0400

    fix composer.json psr-4 namespace mapping.

--- example-composer.php
+++ example-composer.php
@@ -1,16 +1,17 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+ require_once __DIR__ . '/vendor/autoload.php';
 
 use Coveo\Search\SDK\SDKPushPHP\Enum;
 use Coveo\Search\SDK\SDKPushPHP\Constants;
 
-# The below example show that the Enum class is not being loaded.
-class ExampleEnum extends Enum {
+# The below example show that the Enum class being loaded.
+class ExampleEnum extends \Coveo\Search\SDK\SDKPushPHP\Enum {
   const EAMPLE_CONSTANT = 'Hello World!!!';
 }
 echo 'ExampleEnum::EAMPLE_CONSTANT' . PHP_EOL;
 echo ExampleEnum::EAMPLE_CONSTANT;
+echo PHP_EOL;
 
 # The below example show that the Constants class is not being loaded.
 echo '\Coveo\Search\SDK\SDKPushPHP\Constants::DATE_FORMAT_STRING' . PHP_EOL;
